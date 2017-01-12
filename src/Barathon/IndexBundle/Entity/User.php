8d2106d79048e25dffcbcfc48e4c4e515e2b85c8
lzZ00 <310449968@qq.com>
2017-01-12 11:17:51 +0100

    modification des layout séparation des styles dans web/assets/ (création de frontPage/ et userSpace/)

---
+++ src/Barathon/IndexBundle/Entity/User.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lenovo
+ * Date: 2016/12/12
+ * Time: 21:26
+ */
+
+namespace Barathon\IndexBundle\Entity;
+
+
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
